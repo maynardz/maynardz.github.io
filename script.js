6c6e26c3cff3ce60e48209f867b7ba990286da84
Zach Maynard <maynard.zach23@gmail.com>
2020-12-22 12:52:24 -0500

    about

--- script.js
+++ script.js
@@ -69,3 +69,39 @@ anime({
         { value: wave1 },
     ],
 });
+
+
+// ABOUT
+const aboutAction = document.getElementById('about-action');
+const about = document.querySelector('nav');
+
+aboutAction.addEventListener('click', aboutClicked, false);
+
+function aboutClicked() {
+    aboutAction.classList.toggle('active');
+    about.classList.toggle('visible');
+  }
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
+actualWidth = $tip[0].offsetWidth
+actualHeight = $tip[0].offsetHeight
+
+switch (placement) {
+    case 'bottom':
+        tp = { top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2 }
+        break
+    case 'top':
+        tp = { top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2 }
+        break
+    case 'left':
+        tp = { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth }
+        break
+    case 'right':
+        tp = { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width }
+        break
+}
+
+this.applyPlacement(tp, placement)
