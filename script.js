11790c8c7c9270ae8bf51022f43f541e7135ed56
Zach Maynard <maynard.zach23@gmail.com>
2020-12-22 10:29:32 -0500

    change header

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ faders.forEach(fader => {
 sliders.forEach(slider => {
     appearOnScroll.observe(slider);
 })
+
+let test = new Letterize({
+    targets: ".animate-me"
+});
+
+let animation = anime.timeline({
+    targets: test.listAll(),
+    delay: anime.stagger(100, {
+        grid: [test.list()[0].length, test.list().length],
+        from: "center"
+    }),
+    loop: true
+});
+
+animation
+    .add({
+        scale: 0.5
+    })
+    .add({
+        letterSpacing: "10px"
+    })
+    .add({
+        scale: 1
+    })
+    .add({
+        letterSpacing: "6px"
+    });
